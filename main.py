d668216c667b835f529f9e86b039b3cc7448b572
Oltion <cucioltion6@gmail.com>
2026-03-10 20:08:39 +0100

    new project

---
+++ main.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+import psycopg2
+
+import os
+from dotenv import load_dotenv, dotenv_values
+
+# load_dotenv()
+
+# DB_NAME = os.getenv("DB_NAME")
+# DB_USER = os.getenv("DB_USER")
+# DB_PASS = os.getenv("DB_PASS")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+
+config = dotenv_values(".env")
+
+print(config)
+DB_NAME = config["DB_NAME"]
+DB_USER = config["DB_USER"]
+DB_PASS = config["DB_PASS"]
+DB_HOST = config["DB_HOST"]
+DB_PORT = config["DB_PORT"]
+
+try:
+    conn = psycopg2.connect(
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+
+    print(conn)
+    print("Database Connected!")
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT * FROM customers;
+     """)
+    
+    rows = cur.fetchall()
+
+    for data in rows:
+        print(f"ID: {data[0]}")
+        print(f"Name: {data[1]}")
+        print(f"Email: {data[2]}")
+
+    print("-"* 50)
+    print(type(rows))
+    print(rows)
+    print("Data Fetched")
+
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    conn.close()
+    
+    print("Conn closed")
+
+
+# DB_NAME = "JANPython"  # 'Database Name'
+# DB_USER =  "postgres" #"Database User"
+# DB_PASS =  "admin" # "Database Password"
+# DB_HOST =  "localhost" # ose 127.0.0.1 # "Database Host"
+# DB_PORT = "5432"
